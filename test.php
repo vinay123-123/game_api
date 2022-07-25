997c31aafb262465a198f5533525ff205884312b
Vinay Gupta <vinay@inditab.com>
2022-07-25 11:03:37 +0530

    files added to new branch

--- test.php
+++ test.php
@@ -1,5 +1,7 @@
-<?php 
+create new branch in game_api from commnad line.
 
-echo"Vinay Gupta";
+create new branch in https://github.com/inditab-esolutions-pvt-ltd/seoadmin  and upload seoadmin-main from local
 
-?>
+read https://github.com/inditab-esolutions-pvt-ltd/api
+
+Api should be in codeigniter.
